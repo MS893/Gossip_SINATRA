991b7c8e368367593581ebf7ae4c3609080f54af
MS893 <ms893@free.fr>
2025-09-30 07:56:59 +0200

    ajout date

--- lib/comment.rb
+++ lib/comment.rb
@@ -1,17 +1,18 @@
 require 'csv'
 
 class Comment
-  attr_reader :gossip_id, :author, :content
+  attr_reader :gossip_id, :author, :content, :creation_date
 
-  def initialize(gossip_id, author, content)
+  def initialize(gossip_id, author, content, creation_date = Time.now.strftime('%d-%m-%Y'))
     @gossip_id = gossip_id
     @author = author
     @content = content
+    @creation_date = creation_date
   end
 
   def save
     CSV.open('db/comments.csv', 'ab') do |csv|
-      csv << [@gossip_id, @author, @content]
+      csv << [@gossip_id, @author, @content, @creation_date]
     end
   end
 
@@ -21,7 +22,7 @@ class Comment
     File.new('db/comments.csv', 'w').close unless File.exist?('db/comments.csv')
     
     CSV.foreach('db/comments.csv') do |row|
-      comments << Comment.new(row[0], row[1], row[2]) if row[0].to_i == gossip_id.to_i
+      comments << Comment.new(row[0], row[1], row[2], row[3]) if row[0].to_i == gossip_id.to_i
     end
     comments
   end
